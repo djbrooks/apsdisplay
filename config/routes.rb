1712947ed7e9ae201d16edea769ad88f4634b6d1
drewtjohnson <dtjohnson1995@gmail.com>
2018-04-20 12:44:09 -0400

    Redesign

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,17 @@ Rails.application.routes.draw do
   root :to => 'settings#engineering'
   get 'facilities' => 'settings#facilities'
   root :to => 'settings#facilities'
+  get 'cs' => 'settings#cs'
+  root :to => 'settings#cs'
+  get 'hr' => 'settings#hr'
+  root :to => 'settings#hr'
+  get 'accounting' => 'settings#accounting'
+  root :to => 'settings#accounting'
+  get 'wh1' => 'settings#wh1'
+  root :to => 'settings#wh1'
+  get 'wh2' => 'settings#wh2'
+  root :to => 'settings#wh2'
+  get 'production' => 'settings#production'
+  root :to => 'settings#production'
 
 end
